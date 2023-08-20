b13da4f75e03ee51d821208ba8daf683b8bb70c7
micheal <keinesmicheal@gmail.com>
2023-08-20 14:05:45 +0530

    udpate

--- c++/examples/mutex_reference.cc
+++ c++/examples/mutex_reference.cc
@@ -1,5 +1,7 @@
 #include <mutex>
 #include <iostream>
+#include <vector>
+#include <thread>
 
 class GlobalCounter
 {
@@ -7,10 +9,16 @@ class GlobalCounter
 public:
     GlobalCounter(): count(0) {}
     void increment();
+    uint64_t getValue();
 };
 
 void GlobalCounter::increment() {
+    std::cout << "incrementing it with thread id: " << std::this_thread::get_id() << ", count: " << this->count << "\n";
     this->count += 1;
+}
+
+uint64_t GlobalCounter::getValue() {
+    return this->count;
 }
 
 class ProtectedCounter
@@ -24,11 +32,18 @@ class ProtectedCounter
             std::lock_guard<std::mutex> locked_ins(lock_counter);
             this->counter.increment();
         }
-        template<typename F>
-        void custom_increment(F custom_function)
+
+        u_int64_t getValue()
         {
-            std::lock_guard<std::mutex> locked_ins(this->lock_counter);
-            custom_function(this->counter);
+            std::lock_guard<std::mutex> lock_ins(lock_counter);
+            return this->counter.getValue();
+        }
+
+        template<typename F>
+        void custom_increment_func(F custom_user_func)
+        {
+            std::lock_guard<std::mutex> lock_ins(lock_counter);
+            return custom_user_func(this->counter);
         }
 };
 
@@ -48,13 +63,22 @@ void customMaliciousIncrement(GlobalCounter& counter)
 
 ProtectedCounter globalProtectedCounter;
 
-int main()
-{
-    globalProtectedCounter.protected_increment();
-    globalProtectedCounter.custom_increment(customIncrement);
+int main() {
+    std::vector<std::thread> thread_pool;
 
-    // some invalid
-    globalProtectedCounter.custom_increment(customMaliciousIncrement);
-    maliciousPointer->increment();
+    for (int i = 0; i < 10; i++) {
+        thread_pool.emplace_back(std::thread(&ProtectedCounter::protected_increment, &globalProtectedCounter));
+    }
 
+    for (std::thread &thread : thread_pool) {
+        thread.join(); // Wait for all threads to finish
+    }
+
+    std::cout << "Final value: " << globalProtectedCounter.getValue() << std::endl;
+
+    globalProtectedCounter.custom_increment_func(customIncrement);
+
+    std::cout << "Final value after custom increment: " << globalProtectedCounter.getValue() << std::endl;
+
+    return 0;
 }
